63b385e3cadf348f4b892e1acf126587cc80206c
Viincent-Zhong <vincent.zhong@epitech.eu>
2024-09-01 21:44:15 +0200

    [ADD] Monorepo with NuxtJS/TailwindCSS and Nodejs/Express

---
+++ client/nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+  compatibilityDate: '2024-04-03',
+  css: ['~/assets/css/main.css'],
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
