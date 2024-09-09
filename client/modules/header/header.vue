305b5d896c446b6da1d98c8577986b8585c1806b
Viincent-Zhong <vincent.zhong@epitech.eu>
2024-09-09 18:18:09 +0200

    [FIX] Header rooms list not showing properly

--- client/modules/header/header.vue
+++ client/modules/header/header.vue
@@ -4,12 +4,12 @@
         <!-- Search bar -->
         <!-- Posts -->
         <div class="flex flex-col ml-4 lg:ml-24 w-[50%]" @mouseenter="togglePost = true" @mouseleave="togglePost = false">
-            <h1 class="p-6 h-full w-24 text-lg hover:bg-slate-300 rounded-md flex flex-row items-center cursor-default"> Posts </h1>
+            <h1 class="p-6 h-full w-24 text-lg hover:bg-slate-300 rounded-md flex flex-row items-center cursor-default"> Rooms </h1>
             <div v-if="togglePost" class=" bg-slate-300 rounded-md">
                 <!-- 3 by 3 -->
                 <div class="mt-6 pb-6 pr-6 text-zinc-500 rounded-sm font-semibold w-full">
-                    <ul class="grid grid-col-3 p-1">
-                        <li :class="'select-none cursor-pointer lg:p-10 p-2 mb-2 lg:ml-6  hover:bg-slate-400 col-span-1 col-start-' + (index + 1) " v-for="(postMenu, index) in postMenus">
+                    <ul class="grid grid-cols-3 p-1">
+                        <li class="select-none cursor-pointer lg:p-10 p-2 mb-2 lg:ml-6  hover:bg-slate-400" v-for="(postMenu) in postMenus">
                             <!-- Every three index move to a new row -->
                             <h2 class="lg:text-xl text-center" @click="() => moveTo(postMenu.path)"> {{ postMenu.name }} </h2>
                         </li>
