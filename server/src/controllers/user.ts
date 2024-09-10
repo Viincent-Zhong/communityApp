89b00f77e0423045abdf53f45b80d7e313683f1c
Viincent-Zhong <vincent.zhong@epitech.eu>
2024-09-10 22:16:18 +0200

    [ADD] In server update user route

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -61,3 +61,32 @@ export const getUserFromId = async (req: Request, res: Response) => {
         return res.status(500).json({ message: 'Internal server error' });
     }
 }
+
+export const updateUser = async (req: Request, res: Response) => {
+    const authCookie = req.cookies.token;
+    if (!authCookie) {
+        return res.status(401).json({ message: 'Not logged in' });
+    }
+    try {
+        const userData : any = jwt.verify(authCookie, JWT_SECRET);
+        const { name, description } = req.body;
+
+        const user = await prisma.user.update({
+            where: {
+                id: userData.id
+            },
+            data: {
+                name,
+                description
+            },
+        });
+
+        return res.status(200).json(user);
+    } catch (error) {
+        if (error instanceof jwt.TokenExpiredError) {
+            res.clearCookie('token');
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+};
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getUser, getUserFromId } from '../controllers/user';
+import { getUser, getUserFromId, updateUser } from '../controllers/user';
 
 const router = Router();
 
 router.get('/', getUser);
 
 router.get('/unique', getUserFromId);
+
+router.post('/', updateUser);
 
 export default router;
